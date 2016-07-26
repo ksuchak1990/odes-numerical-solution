1c1d736583e66c38c87225113375cb470b59fe5e
ksuchak1990 <ksuchak1990@yahoo.co.uk>
2016-07-26 13:35:21 +0100

    Add numerical solution of SIS on triangle network

--- num_solv.R
+++ num_solv.R
@@ -1,5 +1,7 @@
 th <- 1/3
 library(deSolve)
+
+#### SI triangle network ####
 trinet <- function(t, y, parms) {
   with(as.list(c(y,parms)), {
     dy1 <- 0
@@ -20,6 +22,8 @@ out <- ode (times = times, y = yini, func = trinet, parms = parms)
 png(filename = "./trinet.png")
 plot(out)
 dev.off()
+
+#### SI line network ####
 
 trilin <- function(t, y, parms) {
   with(as.list(c(y,parms)), {
@@ -42,3 +46,25 @@ png(filename = "./trilin.png")
 plot(out)
 dev.off()
 
+#### SIS triangle network ####
+
+sistrilin <- function(t, y, parms) {
+  with(as.list(c(y,parms)), {
+    dy1 <- g * (y[1] + y[2] + y[3])
+    dy2 <- g * (y[5] + y[6]) - (2 * b + g) * y[2]
+    dy3 <- g * (y[5] + y[7]) - (2 * b + g) * y[3]
+    dy4 <- g * (y[6] + y[7]) - (2 * b + g) * y[4]
+    dy5 <- g * y[8] + b * (y[2] + y[3]) - 2 * (b + g) * y[5]
+    dy6 <- g * y[8] + b * (y[2] + y[4]) - 2 * (b + g) * y[6]
+    dy7 <- g * y[8] + b * (y[3] + y[4]) - 2 * (b + g) * y[7]
+    dy8 <- 2 * b * (y[5] + y[6] + y[7]) - 3 * g * y[8]
+    list(c(dy1, dy2, dy3, dy4, dy5, dy6, dy7, dy8))
+  })
+}
+yini <- c(y1 = 0, y2 = th, y3 = th, y4 = th, y5 = 0, y6 = 0, y7 = 0, y8 = 0)
+parms <- c(b=0.5, g=0.01)
+times <- seq(from = 0, to = 20, by = 0.01)
+out <- ode (times = times, y = yini, func = sistrilin, parms = parms)
+png(filename = "./sis_trinet.png")
+plot(out)
+dev.off()
